f96bc57ec4e762d77e24d98b190daf037d7b577e
Mari-Ogawa <38460836+Mari-Ogawa@users.noreply.github.com>
2018-07-09 13:55:03 +0900

    Add files via upload

---
+++ wordpress/main.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(function() {
+    var $allMsg = $('#title');
+    var $wordList = $('#title').html().split("");
+    $('#title').html("");
+    $.each($wordList, function(idx, elem) {
+        var newEL = $("<span/>").text(elem).css({ opacity: 0 });
+        newEL.appendTo($allMsg);
+        newEL.delay(idx * 70);
+        newEL.animate({ opacity: 1 }, 1100);
+    });
+});
--- wordpress/titleLogoAnimation.js
+++ wordpress/titleLogoAnimation.js
@@ -1,3 +1,4 @@
+// JavaScript Document
 window.onload = function(){
 // ページ読み込み時に実行したい処理
 console.log("読み込めているよ");
